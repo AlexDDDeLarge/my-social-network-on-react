daf88030a55db32803c7462424cb7047ef9fa84f
Dmitry Prilipko <dmitry23prilipko@gmail.com>
2021-09-15 08:38:31 +0300

    modified header logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,9 +4,11 @@ import style from './Header.module.css'
 const Header = () => {
   return (
     <header className={style.header}>
+      <div className={style.wrapper}>
         <svg className={style.logo}>
           <use xlinkHref="#reactLogo" x="0" y="-45"></use>
         </svg>
+      </div>
     </header>
   )
 }
